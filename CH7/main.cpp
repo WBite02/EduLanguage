2bd350394e737687797b418b542fb980392e4dcc
tweom <tweom@innotium.com>
2019-12-18 17:51:26 +0900

    Add File Input/output

---
+++ CH7/main.cpp
@@ -0,0 +1,36 @@
+#include <fstream>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ifstream ifs("Hello.txt", std::ios::in);
+
+    if(ifs.is_open())
+    {
+        int intTell = ifs.tellg();
+        cout << "It is tellg pointer : " << intTell << endl;
+        string getString;
+        while(ifs)
+        {
+            getline(ifs, getString);
+            cout << "Print File : " << getString << std::endl;
+        }
+
+    }
+
+    ifs.close();
+
+    ofstream ofs("Hello.txt", std::ios::app | std::ios::out);
+    if (ofs.is_open())
+    {
+        for (int i = 0 ; i < 10 ; i++)
+        {
+            ofs << i;
+            ofs << "Hello World!! Welcome to my world!!";
+        }
+    }
+    
+}
